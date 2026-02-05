03cf591e4d0b5700d875bdc9ef1b94f5eb287278
eaglehat <137303029+eaglehat@users.noreply.github.com>
2026-02-05 21:40:03 +0900

    スペル修正

--- menu_genrater/vite.config.ts
+++ menu_genrater/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/menu_genrater/", // リポジトリ名を / で囲んで指定
+  base: "/menu_generater/", // リポジトリ名を / で囲んで指定
 });
